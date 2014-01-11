d08582539a5f153950f71ef608a9a1d063075cb2
Patrick Mauro <patrick@mauro.ca>
2014-01-11 08:19:39 -0800

    Create uninstall.php
    Create uninstall to delete plugin options.

---
+++ uninstall.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: patrick
+ * Date: 14-01-11
+ * Time: 7:46 AM
+ */
+// if uninstall/delete is not called from WordPress, then exit
+if ( ! defined( 'ABSPATH' ) && ! defined( ! 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit();
+}
+
+// Delete option from options table
+delete_option( BRD_MARKS );
+
+// At this point, one would delete any other option, or custom tables and files
